1bee630b5b0d83112e13218cff5dfc28227f66dd
bellazhong1029 <bellaaa.zjh@gmail.com>
2020-03-23 22:16:15 -0400

    a* is completed

--- A2/pathfinding.py
+++ A2/pathfinding.py
@@ -1,124 +1,19 @@
 import math
 import sys
 
-class Node():
-    """A node class for A* Pathfinding"""
-    def __init__(self, parent=None, position=None):
-        self.parent = parent
-        self.position = position
-        self.g = 0
-        self.h = 0
-        self.f = 0
-
-    def __eq__(self, other):
-        return self.position == other.position
-
-
-
-def astar(maze, start, goal, type):
-    """Returns a list of tuples as a path
-    from the given start to the given goal in the given maze"""
-    # Create start and goal node
-    start_node = Node(None, start)
-    start_node.g = start_node.h = start_node.f = 0
-    goal_node = Node(None, goal)
-    goal_node.g = goal_node.h = goal_node.f = 0
-
-    # Initialize both open and closed list
-    open_list = []
-    closed_list = []
-
-    # Add the start node
-    open_list.append(start_node)
-
-    # Loop until you find the goal
-    while len(open_list) > 0:
-        # Get the current node
-        current_node = open_list[0]
-        current_index = 0
-        for index, item in enumerate(open_list):
-            if item.f < current_node.f:
-                current_node = item
-                current_index = index
-
-        # Pop current off open list, add to closed list
-        open_list.pop(current_index)
-        closed_list.append(current_node)
-
-        # Found the goal
-        if current_node == goal_node:
-            path = []
-            current = current_node
-            while current is not None:
-                path.append(current.position)
-                current = current.parent
-
-            if type == 'a':
-                write_output_to_file_astar("pathfinding_a_out.txt", "Astar", path[::-1],maze)
-            else:
-                write_output_to_file_astar("pathfinding_b_out.txt", "Astar", path[::-1],maze)
-            return path[::-1] # Return reversed path
-
-
-        # Generate children
-        children = []
-        if type == 'a':
-            possible_neighbors = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-        else:
-            possible_neighbors = [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-
-        for new_position in possible_neighbors: # Adjacent squares
-            # Get node position
-            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
-            # Make sure within range
-            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
-                continue
-            # Make sure walkable terrain
-            if maze[node_position[0]][node_position[1]] != 0:
-                continue
-            # Create new node
-            new_node = Node(current_node, node_position)
-            # append
-            children.append(new_node)
-
-        # Loop through children
-        for child in children:
-
-            # Child is on the closed list
-            for closed_child in closed_list:
-                if child == closed_child:
-                    continue
-
-            # Create the f, g, and h values
-            child.g = current_node.g + 1
-            child.h = ((child.position[0] - goal_node.position[0]) ** 2) + ((child.position[1] - goal_node.position[1]) ** 2)
-            child.f = child.g + child.h
-
-            # Child is already in the open list
-            for open_node in open_list:
-                if child == open_node and child.g > open_node.g:
-                    continue
-
-            # Add the child to the open list
-            open_list.append(child)
-
-
-
-
-
-
 '''
-Class GreedyNode
+Class Node
 '''
-class GreedyNode:
-    def __init__(self,x,y,priority,came_from=None):
+class Node:
+    def __init__(self,x,y,priority,came_from=None, cost_so_far = 0):
         self.x = x
         self.y = y
         self.priority = priority
         self.came_from = came_from
+        self.cost_so_far = cost_so_far
 
     '''
-    function neighbors returns all the neighbor nodes
+    Function neighbors returns all the neighbor nodes
     when type == a, the algorithm assumes that the agent can move up, down, left, and right,
     but not diagonally.In best scenerio, the node has four neighbors.
     when type == b, the algorithm assumes that the agent can move up, down, left, and right,
@@ -144,8 +39,17 @@ class GreedyNode:
                     neighbors.append([nodex+1, nodey+1])
         return neighbors
 
+    '''
+    Function is_node compares if two nodes are the same. If two nodes are the same, the function
+    returns True; if two nodes are different, the function returns False
+    '''
     def is_node(self,node):
         return self.x == node[0] and self.y == node[1]
+
+    
+    #Function cost returns the Euclidean distance between two nodes
+    def cost(self, node):
+        return math.sqrt(math.pow(abs(self.x-node[0]),2) + math.pow(abs(self.y-node[1]),2))
 
 
 '''
@@ -157,7 +61,7 @@ When type == b, it assumes that the agent can move up, down, left, and right,
 and diagonally. The cost of moving up, down, left, right or diagonal is 1.
 '''
 def greedySearch(maze ,start, goal,type):
-    startNode = GreedyNode(start[0], start[1],0)
+    startNode = Node(start[0], start[1],0)
     frontier = [startNode]
     visited = [start]
     visitedNode = []
@@ -179,7 +83,7 @@ def greedySearch(maze ,start, goal,type):
             if next not in visited:
                 visited.append(next)
                 priority = heuristic(goal, next)
-                newNode = GreedyNode(next[0], next[1], priority, currentNode)
+                newNode = Node(next[0], next[1], priority, currentNode)
                 frontier.append(newNode)
 
     path = find_path(visitedNode, start, goal)
@@ -193,6 +97,70 @@ def greedySearch(maze ,start, goal,type):
     else:
         write_output_to_file("pathfinding_b_out.txt", "Greedy", path,maze)
 
+
+'''
+Function aStarSearch finds a path between the start position and the goal
+position using A* algorithm.
+When type == a, it assumes that the agent can move up, down, left, and right,
+but not diagonally. The cost of moving up, down, left, or right is 1.
+When type == b, it assumes that the agent can move up, down, left, and right,
+and diagonally. The cost of moving up, down, left, right or diagonal is 1.
+'''
+def aStarSearch(maze ,start, goal,type):
+    startNode = Node(start[0], start[1],0)
+    frontier = [startNode]
+    visited = [start]
+    visitedNode = []
+
+    while len(frontier)!=0:
+        #the code simulates priority queue and pops out the node with the lowest priority
+        min = sys.maxsize
+        for node in frontier:
+            if node.priority < min:
+                currentNode = node
+                min = node.priority
+        frontier.remove(currentNode)
+        visitedNode.append(currentNode)
+        visited.append([currentNode.x, currentNode.y])
+        
+        #searching stops when goal is reached
+        if currentNode.is_node(goal):
+            break
+
+        #loop through all the neighbors 
+        for next in currentNode.neighbors(type,maze):
+            new_cost = currentNode.cost_so_far + currentNode.cost(next)
+            priority = heuristic(goal, next) + new_cost
+            # If the node has been visited but has lesser cost, the path will be replanned
+            if next in visited:
+                nextNode = findNode(visitedNode, next) 
+                if new_cost < nextNode.cost_so_far:
+                    newNode = Node(next[0], next[1], priority, currentNode, new_cost)
+                    frontier.append(newNode)
+            # If the node hasn't been visited
+            else:
+                newNode = Node(next[0], next[1], priority, currentNode, new_cost)
+                frontier.append(newNode)         
+
+    path = find_path(visitedNode, start, goal)
+    #strips down the start node and the goal node
+    if len(path) > 2:
+        path.remove(path[0])
+        path.pop()
+
+    if type == 'a':
+        write_output_to_file("pathfinding_a_out.txt", "A*", path,maze)
+    else:
+        write_output_to_file("pathfinding_b_out.txt", "A*", path,maze)
+
+
+'''
+helper function to find the corresponding node for pair(x,y)
+'''
+def findNode(node_list, target):
+    for node in node_list:
+        if node.x == target[0] and node.y == target[1]:
+            return node
 
 '''
 Heuristic function returns the Euclidean distance between the goal and the given
@@ -246,35 +214,6 @@ def write_output_to_file(filename, algorithm_type, output,maze):
     f.close()
 
 
-
-
-
-
-def write_output_to_file_astar(filename, algorithm_type, output,maze):
-    path = []
-    with open(filename,"a+") as f:
-        f.write("%s\n"%(algorithm_type))
-        # for i in output:
-        #     maze[i.y-1][i.x-1] = 'P'
-        for line in range(len(maze)):
-            for index in range (len(maze[line])):
-                item = maze[line][index]
-                if item == 0:
-                    maze[line][index] = "_"
-                elif item == 1:
-                    maze[line][index] = "X"
-        for path in output:
-            x = path[1]-1
-            y = path[0]
-            maze[x][y]="P"
-        for line in maze:
-            for chac in line:
-                f.write("%c"%(chac))
-            f.write("\n")
-    f.close()
-
-
-
 '''
 Helper function of build_maze
 '''
@@ -298,26 +237,6 @@ def covert_file_to_matrix(count, line, maze):
             goal = [line.index(i)+1, count]
     maze.append(newline)
 
-def covert_file_to_matrix_aStar(count, line, maze):
-    global start, goal
-    newline= []
-    for i in line:
-        # obstacle normal block= 1
-        if i =='X':
-            newline.append(1)
-        # non-obstavle normal block = 0
-        elif i =='_':
-            newline.append(0)
-        # start block
-        elif i =='S':
-            newline.append('S')
-            start = (line.index(i)+1, count)
-        # goal block
-        elif i=='G':
-            newline.append('G')
-            goal = (line.index(i)+1, count)
-    maze.append(newline)
-
 
 '''
 Function build_maze constructs a matrix from a graph maze
@@ -339,32 +258,16 @@ def build_maze(filepath):
             line = f.readline()
             count += 1
 
-def build_maze_astar(filepath):
-    global maze, startastar, goalastar
-    maze=[]
-    startastar = ()
-    goalastar = ()
-
-    with open(filepath) as f:
-        line = f.readline()
-        count=1
-        while line:
-            maze_line = line.strip()
-            covert_file_to_matrix_aStar(count, maze_line, maze)
-            line = f.readline()
-            count += 1
-
 
 def main():
-    # build_maze("pathfinding_a.txt")
-    # greedySearch(maze, start, goal, 'a')
-    # build_maze("pathfinding_b.txt")
-    # greedySearch(maze, start, goal, 'b')
+    build_maze("pathfinding_a.txt")
+    greedySearch(maze, start, goal, 'a')
+    build_maze("pathfinding_b.txt")
+    greedySearch(maze, start, goal, 'b')
 
-    build_maze_astar("pathfinding_a.txt")
-    result = astar(maze, start, goal, 'a')
-    print(result)
-    build_maze_astar("pathfinding_b.txt")
-    astar(maze, start, goal, 'b')
+    build_maze("pathfinding_a.txt")
+    aStarSearch(maze, start, goal, 'a')
+    build_maze("pathfinding_b.txt")
+    aStarSearch(maze, start, goal, 'b')
 
 main()
